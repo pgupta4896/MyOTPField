1c6da18b71402c2b59dd2682a06eae2b81723c3d
prakhar.unofficial <a1@MacBook-Pro.local>
2024-08-18 22:48:40 +0530

    initial commit

---
+++ Readme.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  MyOtpView
+//
+//  Created by prakhar.unofficial on 18/08/24.
+//
+
+# SwiftUI OTP Field
+
+A customizable OTP (One-Time Password) input field component for SwiftUI applications. Supports variable lengths and styles.
+
+## Features
+
+- Supports any number of OTP fields.
+- Customizable styles: rounded background or bottom underline.
+- Customizable colors for active/inactive states and background.
+- Automatically manages focus between fields.
+- Easy integration with SwiftUI projects.
+
+## Installation
+
+### Swift Package Manager
+
+Add the following to your `Package.swift` file:
+
+```swift
+dependencies: [
+    .package(url: "https://github.com/yourusername/SwiftUIOTPField.git", from: "1.0.0")
+]
