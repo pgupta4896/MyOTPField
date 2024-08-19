793ee81047501b37e2365520aa62fb24721f78a4
prakhar gupta <90167769+pgupta4896@users.noreply.github.com>
2024-08-19 08:25:31 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,19 +5,32 @@ import PackageDescription
 
 let package = Package(
     name: "MyOtpView",
+    platforms: [
+        .iOS(.v13), // Specify the minimum iOS version required for your package
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyOtpView",
             targets: ["MyOtpView"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // Add any dependencies here if needed, for example:
+        // .package(url: "https://github.com/another/package.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyOtpView"),
+            name: "MyOtpView",
+            dependencies: [],
+            path: "Sources/MyOtpView"
+        ),
         .testTarget(
             name: "MyOtpViewTests",
-            dependencies: ["MyOtpView"]),
+            dependencies: ["MyOtpView"],
+            path: "Tests/MyOtpViewTests"
+        ),
     ]
 )
